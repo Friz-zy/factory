b260e6b73aedec57041066e7f1bd42180394e7b8
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2014-06-21 22:55:22 +0300

    add factory.py as first sketches

---
+++ factory.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""Factory provides api for local and nonlocal running of functions,scripts, etc via shh and *sh."""
+"""
+Copyright (c) by Filipp Kucheryavy aka Frizzy <filipp.s.frizzy@gmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted 
+provided that the following conditions are met:
+
+a. Redistributions of source code must retain the above copyright notice, this list of 
+conditions and the following disclaimer. 
+
+b. Redistributions in binary form must reproduce the above copyright notice, this list of 
+conditions and the following disclaimer in the documentation and/or other materials provided 
+with the distribution. 
+
+c. Neither the name of the nor the names of its contributors may be used to endorse or promote 
+products derived from this software without specific prior written permission. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+""" 
+__author__ = 'Filipp Frizzy'
+__credits__ = ["Filipp Frizzy"]
+__license__ = "MIT"
+__version__ = ""
+__maintainer__ = "Filipp Frizzy"
+__email__ = "filipp.s.frizzy@gmail.com"
+__status__ = "Development"
+
+import os
+import sys
+import logging
+import subprocess
+
+# default variables
+
+def run(command, use_sudo=False, user='', group=''):
+    """
+    """
+    # run as root
+    if use_sudo:
+        if not run_as_root:
+            if 'sudo' not in command.split():
+                command = " ".join(('sudo', command))
+    # switching user
+    if user:
+        if 'sudo' not in command.split():
+            command = " ".join(('sudo -u %s -s' % user, command))
+        else:
+            command.replace('sudo', 'sudo -u %s -s' % user)
+    # switching group
+    if group:
+        if 'sudo' not in command.split():
+            command = " ".join(('sudo -g %s -s' % group, command))
+        else:
+            command.replace('sudo', 'sudo -g %s -s' % group)
+    pass
+
+def sudo(command, user='', group=''):
+    """sudo is alias for run(use_sudo=True)"""
+    return run(command, use_sudo=True, user=user, group=group)
+
+def check_is_root():
+    return not run('id -u')
