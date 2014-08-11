26efb181a599f30674b22435268a69bac6349c44
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2014-08-11 22:15:18 +0300

    tests/test_factory_specification.py: refactoring

--- tests/test_factory_specification.py
+++ tests/test_factory_specification.py
@@ -24,94 +24,93 @@ factory = imp.load_source(
 )
 
 
-def test_should_write_command_stdout_to_sys_stdout(capsys):
-    sys.argv = ['factory.py', "run:echo 'hello world!'"]
-    factory.main()
-    out, err = capsys.readouterr()
-    assert "hello world!" in out
+class TestAPI:
+    def test_should_execute_run_function(self, capsys):
+        sys.argv = ['factory.py', "-r", "echo 'hello world!'"]
+        factory.main()
+        out, err = capsys.readouterr()
+        assert "hello world!" in out
+
+    def test_should_execute_sudo_function(self, capsys):
+        sys.argv = ['factory.py', "-s", "echo 'hello world!'"]
+        factory.main()
+        out, err = capsys.readouterr()
+        assert "hello world!" in out
+
+    def test_should_execute_push_function(self, tmpdir, a, b, capsys):
+        sys.argv = ['factory.py', "push", a, b]
+        factory.main()
+        with open(a, 'r') as a, open(b, 'r') as b:
+            assert a.read() == b.read()
 
 
-def test_should_write_command_stderr_to_sys_stderr(capsys):
-    sys.argv = ['factory.py', 'run:qwertyuiop']
-    factory.main()
-    out, err = capsys.readouterr()
-    assert "/bin/sh: 1: qwertyuiop: not found" in err
-    
-
-def test_should_parse_splited_arguments(capsys):
-    sys.argv = ['factory.py', "run", "echo 'hello world!'"]
-    factory.main()
-    out, err = capsys.readouterr()
-    assert "hello world!" in out
+    def test_should_execute_pull_function(self, tmpdir, a, b, capsys):
+        sys.argv = ['factory.py', "pull", a, b]
+        factory.main()
+        with open(a, 'r') as a, open(b, 'r') as b:
+            assert a.read() == b.read()
 
 
-def test_should_execute_run_function(capsys):
-    sys.argv = ['factory.py', "-r", "echo 'hello world!'"]
-    factory.main()
-    out, err = capsys.readouterr()
-    assert "hello world!" in out
+class TestArgParsing:
+    def test_should_parse_splited_arguments(self, capsys):
+        sys.argv = ['factory.py', "run", "echo 'hello world!'"]
+        factory.main()
+        out, err = capsys.readouterr()
+        assert "hello world!" in out
+
+    def test_should_execute_many_functions(self, capsys):
+        sys.argv = ['factory.py', "run", "echo 'hello world!'", "run", "echo 'hello world!'"]
+        factory.main()
+        out, err = capsys.readouterr()
+        assert out.count('hello world!') == 2
+
+    def test_should_parse_many_arguments(self, capsys):
+        sys.argv = ['factory.py', "-nr", "echo 'hello world!'"]
+        factory.main()
+        out, err = capsys.readouterr()
+        assert out == '' and err == ''
+        with open('factory.log', 'r') as f:
+            assert "hello world!" in f.readlines()[-1]
 
 
-def test_should_execute_sudo_function(capsys):
-    sys.argv = ['factory.py', "-s", "echo 'hello world!'"]
-    factory.main()
-    out, err = capsys.readouterr()
-    assert "hello world!" in out
+class TestFeedback:
+    def test_should_write_command_stdout_to_sys_stdout(self, capsys):
+        sys.argv = ['factory.py', "run:echo 'hello world!'"]
+        factory.main()
+        out, err = capsys.readouterr()
+        assert "hello world!" in out
 
 
-def test_should_execute_push_function(tmpdir, a, b, capsys):
-    sys.argv = ['factory.py', "--push", a, b]
-    factory.main()
-    with open(a, 'r') as a, open(b, 'r') as b:
-        assert a.read() == b.read()
+    def test_should_write_command_stderr_to_sys_stderr(self, capsys):
+        sys.argv = ['factory.py', 'run:qwertyuiop']
+        factory.main()
+        out, err = capsys.readouterr()
+        assert "/bin/sh: 1: qwertyuiop: not found" in err
 
 
-def test_should_execute_pull_function(tmpdir, a, b, capsys):
-    sys.argv = ['factory.py', "--pull", a, b]
-    factory.main()
-    with open(a, 'r') as a, open(b, 'r') as b:
-        assert a.read() == b.read()
+    def test_should_communicate_with_stdin(self, capsys):
+        sys.argv = ['factory.py', "sh"]
+        sys.stdin.write("echo 'hello world!'; exit")
+        sys.stdin.flush()
+        factory.main()
+        out, err = capsys.readouterr()
+        assert "hello world!" in out
 
 
-def test_should_execute_many_functions(capsys):
-    sys.argv = ['factory.py', "run", "echo 'hello world!'", "run", "echo 'hello world!'"]
-    factory.main()
-    out, err = capsys.readouterr()
-    assert out.count('hello world!') == 2
+    def test_should_write_logs(self):
+        sys.argv = ['factory.py', "run", "echo 'hello world!'"]
+        factory.main()
+        with open('factory.log', 'r') as f:
+            assert "hello world!" in f.readlines()[-1]
 
 
-def test_should_communicate_with_stdin(capsys):
-    sys.argv = ['factory.py', "sh"]
-    sys.stdin.write("echo 'hello world!'; exit")
-    sys.stdin.flush()
-    factory.main()
-    out, err = capsys.readouterr()
-    assert "hello world!" in out
-
-
-def test_should_write_logs():
-    sys.argv = ['factory.py', "run", "echo 'hello world!'"]
-    factory.main()
-    with open('factory.log', 'r') as f:
-        assert "hello world!" in f.readlines()[-1]
-
-
-def test_should_only_write_logs(capsys):
-    sys.argv = ['factory.py', "-n", "run", "echo 'hello world!'"]
-    factory.main()
-    out, err = capsys.readouterr()
-    assert out == '' and err == ''
-    with open('factory.log', 'r') as f:
-        assert "hello world!" in f.readlines()[-1]
-
-
-def test_should_parse_many_arguments(capsys):
-    sys.argv = ['factory.py', "-nr", "echo 'hello world!'"]
-    factory.main()
-    out, err = capsys.readouterr()
-    assert out == '' and err == ''
-    with open('factory.log', 'r') as f:
-        assert "hello world!" in f.readlines()[-1]
+    def test_should_only_write_logs(self, capsys):
+        sys.argv = ['factory.py', "-n", "run", "echo 'hello world!'"]
+        factory.main()
+        out, err = capsys.readouterr()
+        assert out == '' and err == ''
+        with open('factory.log', 'r') as f:
+            assert "hello world!" in f.readlines()[-1]
 
 
 if __name__ == '__main__':
