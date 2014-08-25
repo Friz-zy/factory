447e776350ab4c84f85499f81154e096460482ff
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2014-08-25 22:24:36 +0300

    factory.py: add docstring to class set_connect_env

--- factory.py
+++ factory.py
@@ -469,11 +469,46 @@ def stdin_loop():
 
 
 class set_connect_env():
+    """Context manager that set connect_env atributes.
+
+    Connect_env attibutes:
+      connect_string (str): [user@]host[:port]
+      user (str): username for connect
+      host (str): hostname or ip for connect
+      port (str): port for connect
+      con_args (str): options for ssh
+      logger (logging.logger object): logger object for this connect
+
+    Examples:
+      >>> load_config()
+      >>> with set_connect_env('user@host:port', '') as connect_env:
+      ...     connect_env.__dict__ # doctest: +NORMALIZE_WHITESPACE
+      {'connect_string': 'user@host:port',
+      'con_args': '',
+      'host': 'host',
+      'user': 'user',
+      'logger': ...,
+      'port': 'port'}
+
+    """
     def __init__(self, connect_string, con_args=''):
+        """Initializing of  set_connect_env class.
+
+        Args:
+          connect_string (str): [user@]host[:port]
+          con_args (str): options for ssh
+
+        """
         self.cs = connect_string
         self.ca = con_args
 
     def __enter__(self):
+        """Set atributes to connect_env object.
+
+        Returns:
+          connect_env object with most of all atributes
+
+        """
         connect_string = self.cs
         con_args = self.ca
         connect_env.connect_string = connect_string
@@ -500,6 +535,11 @@ class set_connect_env():
         return connect_env
 
     def __exit__(self, type, value, traceback):
+        """Reinitialized global connect_env as Empty class.
+
+        FIXME: is it really works?! I doesn't think so...
+
+        """
         connect_env = Empty()
 
 
