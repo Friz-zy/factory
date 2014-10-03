dd78533374a0912773c25e6d4a533953e02a9760
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2014-10-03 23:41:00 +0300

    factory: update api

--- factory/api.py
+++ factory/api.py
@@ -5,5 +5,5 @@
 # This file is part of https://github.com/Friz-zy/factory
 
 from operations import push, pull, put, get, run, sudo, local, open_shell, run_script, check_is_root
-from main import logging, global_env, connect_env
+from main import logging, global_env, connect_env, stdin_loop
 from  main import global_env as env
