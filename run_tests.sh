4f4250c1e7be9e0a5c1f361a0187719047b740fb
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2014-10-25 02:23:17 +0300

    run_tests.sh: revrite run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,16 @@ if [ ! -d "env" ]; then
   virtualenv 'env'
 fi
 
-/bin/bash -c ". env/bin/activate; python setup.py install; cd tests; py.test -v; yes | pip uninstall factory" &&
+rm -rf build/ dist/ Factory.egg-info/
+export VIRTUAL_ENV="env"
+export PATH="$VIRTUAL_ENV/bin:$PATH"
+unset PYTHON_HOME
+source env/bin/activate
+export PYTHONPATH=.
+python setup.py install
+cd tests; py.test -v
+yes | pip uninstall factory
+deactivate
+cd ..
 rm -rf build/ dist/ Factory.egg-info/
 
